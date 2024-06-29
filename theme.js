9d5cf4e107559356fc488ec12ef340b8d510a8dc
Dev.Huang <18778418865@163.com>
2024-06-29 13:41:56 +0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,17 +1,17 @@
 // 检测系统主题模式
-        function detectColorScheme() {
-            const darkSchemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-            if (darkSchemeMediaQuery.matches) {
-                document.body.classList.add('dark-mode');
-                document.body.classList.remove('light-mode');
-            } else {
-                document.body.classList.add('light-mode');
-                document.body.classList.remove('dark-mode');
-            }
-        }
+function detectColorScheme() {
+    const darkSchemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    if (darkSchemeMediaQuery.matches) {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+    } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+    }
+}
 
-        // 监听主题模式变化
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', detectColorScheme);
+// 监听主题模式变化
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', detectColorScheme);
 
-        // 初始化检测主题模式
-        detectColorScheme();
+// 初始化检测主题模式
+detectColorScheme();
